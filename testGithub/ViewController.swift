70cfe88aa9b10d63801e9794db21c046edbef890
aayushj567 <aayushjaiswal765@gmail.com>
2024-04-08 14:18:58 -0400

    initial commmit

---
+++ testGithub/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  testGithub
+//
+//  Created by Aayush Jaiswal on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set background color to white
+        view.backgroundColor = .white
+        
+        // Add a title
+        let titleLabel = UILabel()
+        titleLabel.text = "My Title"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        // Add constraints for the title label
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+
